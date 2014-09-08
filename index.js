8323b1766e4bee28e3649f4ac7e463f89eefe555
MMendes999 <mario.c.mendes1985@gmail.com>
2014-09-08 14:16:57 +0100

    updates index.html, css.main and index.js

--- index.js
+++ index.js
@@ -10,19 +10,75 @@ var routes =[
     method: 'GET',
     handler: function (req, reply){
 
-        request('http://twitter.com/' + req.params.username, function(error, response, body){
+      request('http://twitter.com/' + req.params.username, function(error, response, body){
         console.log(response.statusCode);
 
         if ( response.statusCode === 404 ) {
-          console.log( "Username " + req.params.username + " is available!" );
+          console.log( "Username " + req.params.username + " is available on Twitter" );
           reply({"available":"yes"});
         }
 
         if ( response.statusCode === 200 ) {
-          console.log( "Username " + req.params.username + " is already taken!" );
+          console.log( "Username " + req.params.username + " is already taken on Twitter" );
           reply({"available":"no"});
         }
       });
+
+      request('http://github.com/' + req.params.username, function(error, response, body){
+        console.log(response.statusCode);
+
+        if ( response.statusCode === 404 ) {
+          console.log( "Username " + req.params.username + " is available on Github" );
+          reply({"available":"yes"});
+        }
+
+        if ( response.statusCode === 200 ) {
+          console.log( "Username " + req.params.username + " is already taken on Github" );
+          reply({"available":"no"});
+        }
+      });
+
+    request('http://pinterest.com/' + req.params.username, function(error, response, body){
+      console.log(response.statusCode);
+
+      if ( response.statusCode === 404 ) {
+        console.log( "Username " + req.params.username + " is available on Pinterest" );
+        reply({"available":"yes"});
+      }
+
+      if ( response.statusCode === 200 ) {
+        console.log( "Username " + req.params.username + " is already taken on Pinterest" );
+        reply({"available":"no"});
+      }
+    });
+
+    request('http://facebook.com/' + req.params.username, function(error, response, body){
+      console.log(response.statusCode);
+
+      if ( response.statusCode === 404 ) {
+        console.log( "Username " + req.params.username + " is available on Facebook" );
+        reply({"available":"yes"});
+      }
+
+      if ( response.statusCode === 200 ) {
+        console.log( "Username " + req.params.username + " is already taken on Facebook" );
+        reply({"available":"no"});
+      }
+    });
+
+    request('http://instagram.com/' + req.params.username, function(error, response, body){
+      console.log(response.statusCode);
+
+      if ( response.statusCode === 404 ) {
+        console.log( "Username " + req.params.username + " is available on Instagram" );
+        reply({"available":"yes"});
+      }
+
+      if ( response.statusCode === 200 ) {
+        console.log( "Username " + req.params.username + " is already taken on Instagram" );
+        reply({"available":"no"});
+      }
+    });
     }
   }
 ];
---
+++ scripts/hideReveal.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+  $("#searchresults").addClass('hide');
+});
