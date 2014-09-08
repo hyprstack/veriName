ee199c425148805bc836e18888f38aabf25e4d32
MMendes999 <mario.c.mendes1985@gmail.com>
2014-09-08 15:54:26 +0100

    updates index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var routes =[
     method: 'GET',
     handler: function (req, reply){
 
-      request('http://twitter.com/' + req.params.username, function(error, response, body){
+      request('http://' + req.hostname + '/' + req.params.username, function(error, response, body){
         console.log(response.statusCode);
 
         if ( response.statusCode === 404 ) {
@@ -23,62 +23,6 @@ var routes =[
           reply({"available":"no"});
         }
       });
-
-      request('http://github.com/' + req.params.username, function(error, response, body){
-        console.log(response.statusCode);
-
-        if ( response.statusCode === 404 ) {
-          console.log( "Username " + req.params.username + " is available on Github" );
-          reply({"available":"yes"});
-        }
-
-        if ( response.statusCode === 200 ) {
-          console.log( "Username " + req.params.username + " is already taken on Github" );
-          reply({"available":"no"});
-        }
-      });
-
-    request('http://pinterest.com/' + req.params.username, function(error, response, body){
-      console.log(response.statusCode);
-
-      if ( response.statusCode === 404 ) {
-        console.log( "Username " + req.params.username + " is available on Pinterest" );
-        reply({"available":"yes"});
-      }
-
-      if ( response.statusCode === 200 ) {
-        console.log( "Username " + req.params.username + " is already taken on Pinterest" );
-        reply({"available":"no"});
-      }
-    });
-
-    request('http://facebook.com/' + req.params.username, function(error, response, body){
-      console.log(response.statusCode);
-
-      if ( response.statusCode === 404 ) {
-        console.log( "Username " + req.params.username + " is available on Facebook" );
-        reply({"available":"yes"});
-      }
-
-      if ( response.statusCode === 200 ) {
-        console.log( "Username " + req.params.username + " is already taken on Facebook" );
-        reply({"available":"no"});
-      }
-    });
-
-    request('http://instagram.com/' + req.params.username, function(error, response, body){
-      console.log(response.statusCode);
-
-      if ( response.statusCode === 404 ) {
-        console.log( "Username " + req.params.username + " is available on Instagram" );
-        reply({"available":"yes"});
-      }
-
-      if ( response.statusCode === 200 ) {
-        console.log( "Username " + req.params.username + " is already taken on Instagram" );
-        reply({"available":"no"});
-      }
-    });
     }
   }
 ];
