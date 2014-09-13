713a66553564067a688a21568da4a01a451b7f0d
MMendes999 <mario.c.mendes1985@gmail.com>
2014-09-13 18:43:03 +0100

    updates relative url to results.js

--- static/scripts/results.js
+++ static/scripts/results.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     var username = $("#searchname").val().trim(); // store value from searchbox
     console.log(username);
     //send ajax request to server to check for username availability
-    $.get("http://localhost:8080/"+username, username, function(data){
+    $.get("/"+username, username, function(data){
       console.log(data);
     });
   });
