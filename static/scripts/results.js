38bc97c5d7f57d60d7b7627596c30c08070a8974
MMendes999 <mario.c.mendes1985@gmail.com>
2014-09-14 19:58:38 +0100

    updates index.html and results.js

--- static/scripts/results.js
+++ static/scripts/results.js
@@ -1,17 +1,21 @@
 $(document).ready(function(){
   console.log("Ready!");
 
-  $("#searchbutton").on('click', function(){
+  $("#searchbutton").on('click', function(event){
+
     var username = $("#searchname").val().trim(); // store value from searchbox
     console.log(username);
 
-    if(username){
-      $("#searchresults").css("display", "block"); //reveal searchresults
-      var html = "<p>";
-      html += username;
-      html += "</p>";
-      $(".username").prepend(html); // this has the nasty effect of continuously adding the search terms to the html
+    if(username === ""){
+      event.preventDefault();
+    }
 
+    if(username){
+      var newhtml = "<p>";
+      newhtml += username;
+      newhtml += "</p>";
+      $(".username").html(newhtml); // this has the nasty effect of continuously adding the search terms to the html
+      $(".username").remove("newhtml");
       //send ajax request to server to check for username availability
       $.get("/"+username, username, function(data){
         console.log(data);
